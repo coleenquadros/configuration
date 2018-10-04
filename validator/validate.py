fe0695c3a85f0c2ebd10e5139096ce83a068bbce
Jaime Melis <jmelis@redhat.com>
2018-10-04 10:37:41 +0200

    Refactor as per @jzelinsk comments
    - Doesn't use OO for ValidateFile
    - Uses comprehensions
    - fetch_schema_file handles specifically local file schemas
    - Uses `.format` instead of the `%` syntax
    - Formatting (double line after imports)
    - MissingSchemaFile keeps missing filepath
    - Output format changed from a list to a dict

--- validator/validate.py
+++ validator/validate.py
@@ -18,152 +18,133 @@ logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 
 class MissingSchemaFile(Exception):
-    pass
-
-
-class InvalidSchemaUrl(Exception):
-    pass
+    def __init__(self, path):
+        self.path = path
+        message = "file not found: {}".format(path)
+        super(Exception, self).__init__(message)
 
 
 class ValidationResult(object):
     def summary(self):
         status = 'OK' if self.status else 'ERROR'
-        return "%s: %s (%s)" % (status, self.filename, self.schema_url)
+        return "{}: {} ({})".format(status, self.filename, self.schema_url)
 
 
-class ValidationResultOK(ValidationResult):
+class ValidationOK(ValidationResult):
     status = True
 
     def __init__(self, filename, schema_url):
         self.filename = filename
         self.schema_url = schema_url
 
-    def dump_obj(self):
+    def dump(self):
         return {
-            self.filename: {
-                "status": "OK",
-                "schema_url": self.schema_url,
-            }
+            "summary": self.summary(),
+            "status": "OK",
+            "schema_url": self.schema_url,
         }
 
 
-class ValidationResultError(ValidationResult):
+class ValidationError(ValidationResult):
     status = False
 
-    def __init__(self, filename, schema_url, reason, error):
+    def __init__(self, filename, reason, error, schema_url=None):
         self.filename = filename
-        self.schema_url = schema_url
         self.reason = reason
         self.error = error
+        self.schema_url = schema_url
+
+    def dump(self):
+        return {
+            "summary": self.summary(),
+            "status": "ERROR",
+            "schema_url": self.schema_url,
+            "reason": self.reason,
+            "error": self.error.message
+        }
 
     def error_info(self):
         if self.error.message:
-            msg = "%s\n%s" % (self.reason, self.error.message)
+            msg = "{}\n{}".format(self.reason, self.error.message)
         else:
             msg = self.reason
 
         return msg
 
-    def dump_obj(self):
-        return {
-            self.filename: {
-                "status": "ERROR",
-                "schema_url": self.schema_url,
-                "reason": self.reason,
-                "error": self.error.message
-            }
-        }
+
+def validate_file(filename):
+    logging.info('validating: {}'.format(filename))
+
+    try:
+        data = anymarkup.parse_file(filename)
+    except anymarkup.AnyMarkupError as e:
+        return ValidationError(filename, "FILE_PARSE_ERROR", e)
+
+    try:
+        schema_url = data[u'$schema']
+    except KeyError as e:
+        return ValidationError(filename, "MISSING_SCHEMA_URL", e)
+
+    try:
+        schema = fetch_schema(schema_url)
+    except MissingSchemaFile as e:
+        return ValidationError(filename, "MISSING_SCHEMA_FILE", e, schema_url)
+    except requests.HTTPError as e:
+        return ValidationError(filename, "HTTP_ERROR", e, schema_url)
+    except anymarkup.AnyMarkupError as e:
+        return ValidationError(filename, "SCHEMA_PARSE_ERROR", e, schema_url)
+
+    try:
+        jsonschema.validate(data, schema)
+    except jsonschema.ValidationError as e:
+        return ValidationError(filename, "VALIDATION_ERROR", e, schema_url)
+
+    return ValidationOK(filename, schema_url)
 
 
-class ValidateFile(object):
+def fetch_schema(schema_url):
+    if schema_url.startswith('http'):
+        r = requests.get(schema_url)
+        r.raise_for_status()
+        schema = r.text
+    else:
+        schema = fetch_schema_file(schema_url)
 
-    """path to the filename to validate"""
-    filename = None
+    return anymarkup.parse(schema)
 
-    """schema url as it appears in the file to validate"""
-    schema_url = None
 
-    def __init__(self, filename):
-        self.filename = filename
+def fetch_schema_file(schema_url):
+    if schema_url[0] == '/':
+        schema_url = schema_url[1:]
 
-    def validate(self):
-        try:
-            data = anymarkup.parse_file(self.filename)
-        except anymarkup.AnyMarkupError as e:
-            return self._error("FILE_PARSE_ERROR", e)
+    schema_file = os.path.join(APP_ROOT, schema_url)
 
-        try:
-            self.schema_url = data[u'$schema']
-        except KeyError as e:
-            return self._error("MISSING_SCHEMA_URL", e)
+    if not os.path.isfile(schema_file):
+        raise MissingSchemaFile(schema_file)
 
-        try:
-            schema = self.get_schema()
-        except MissingSchemaFile as e:
-            return self._error("MISSING_SCHEMA_FILE", e)
-        except requests.HTTPError as e:
-            return self._error("HTTP_ERROR", e)
-        except anymarkup.AnyMarkupError as e:
-            return self._error("SCHEMA_PARSE_ERROR", e)
+    with open(schema_file, 'r') as f:
+        schema = f.read()
 
-        try:
-            jsonschema.validate(data, schema)
-        except jsonschema.ValidationError as e:
-            return self._error("VALIDATION_ERROR", e)
-
-        return self._ok()
-
-    def get_schema(self):
-        if self.schema_url[0] == '/':
-            schema_url = self.schema_url[1:]
-            schema_file = os.path.join(APP_ROOT, schema_url)
-
-            if not os.path.isfile(schema_file):
-                raise MissingSchemaFile()
-
-            with open(schema_file, 'r') as f:
-                schema = f.read()
-        elif schema_url.startswith('http'):
-            r = requests.get(schema_url)
-            r.raise_for_status()
-            schema = r.text
-        else:
-            raise InvalidSchemaUrl()
-
-        return anymarkup.parse(schema)
-
-    def _error(self, reason, error):
-        return ValidationResultError(self.filename, self.schema_url, reason,
-                                     error)
-
-    def _ok(self):
-        return ValidationResultOK(self.filename, self.schema_url)
+    return schema
 
 
 def main():
-    filenames = []
-    for arg in sys.argv[1:]:
-        filenames.extend(glob.glob(arg))
+    results = {
+        path: validate_file(path).dump()
+        for arg in sys.argv[1:]
+        for path in glob.glob(arg)
+        if os.path.isfile(path) and re.search("\.(json|ya?ml)$", path)
+    }
 
-    results = []
-    results_error_output = []
-    for filename in filenames:
-        is_file = os.path.isfile(filename)
-        is_markup = re.search("\.(json|ya?ml)$", filename)
+    errors = [
+        r['status']
+        for _, r in results.items()
+        if r['status'] == 'ERROR'
+    ]
 
-        if is_file and is_markup:
-            v = ValidateFile(filename)
-            validation_result = v.validate()
-            logging.info(validation_result.summary())
-            results.append(validation_result)
+    print json.dumps(results)
 
-            if not validation_result.status:
-                results_error_output.append(validation_result.dump_obj())
-
-    if len(results_error_output) == 0:
-        sys.exit(0)
-    else:
-        print json.dumps(results_error_output)
+    if len(errors) > 0:
         sys.exit(1)
 
 
