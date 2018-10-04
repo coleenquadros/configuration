f48420af99770102ada14231808b78722f369bdb
Jaime Melis <jmelis@redhat.com>
2018-10-04 12:42:50 +0200

    Add css

--- pr_check.sh
+++ pr_check.sh
@@ -5,8 +5,6 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-mkdir -p reports
-
 APP_ROOT=$(pwd) python validator/validate.py 'services/**/*' > results.json
 exit_status=$?
 
