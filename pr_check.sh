cb875186d858124e1a5f74cdad8f9732c02f2188
Jaime Melis <jmelis@redhat.com>
2018-10-05 13:26:22 +0200

    typo

--- pr_check.sh
+++ pr_check.sh
@@ -5,7 +5,7 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-PP_ROOT=$(pwd) python validator/validate.py 'services/**/*' > results.json
+APP_ROOT=$(pwd) python validator/validate.py 'services/**/*' > results.json
 exit_status=$?
 
 python gen_report.py results.json > reports/index.html
