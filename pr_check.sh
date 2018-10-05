77de687c35272df89578e69a148f1d900ad3d586
Jaime Melis <jmelis@redhat.com>
2018-10-05 12:57:06 +0200

    Validate only services
    schemas are implicitely checked

--- pr_check.sh
+++ pr_check.sh
@@ -5,7 +5,7 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-APP_ROOT=$(pwd) python validator/validate.py 'schemas/**/*' 'services/**/*' > results.json
+PP_ROOT=$(pwd) python validator/validate.py 'services/**/*' > results.json
 exit_status=$?
 
 python gen_report.py results.json > reports/index.html
