6e7d4a6e1e34d09d92c96ab9ccb8694f6b4e057f
Jaime Melis <jmelis@redhat.com>
2018-10-22 18:26:48 +0200

    be verbose about the report generation

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+REPORT="reports/index.html"
+
 export DESCRIPTION=$(
     curl -sk "$BUILD_URL/api/json" | \
     jq .description | \
@@ -12,5 +14,6 @@ make validate > reports/results.json
 exit_status=$?
 
 python gen_report.py reports/results.json > reports/index.html
+echo "Report written to: ${REPORT}"
 
 exit $exit_status
