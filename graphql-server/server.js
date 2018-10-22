3cf903b13bc6521780cb5abc89329fd945e0bcb2
Jaime Melis <jmelis@redhat.com>
2018-10-22 16:40:11 +0200

    refactor loading of schemas

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -1,21 +1,24 @@
 const { GraphQLServer } = require('graphql-yoga')
 const merge = require('lodash/merge');
 
-const base = require('./schema/base');
-const user = require('./schema/user');
-const users = require('./schema/users');
+var schemaFiles = [
+    'base',
+    'user',
+    'users',
+]
+
+var typeDefs = [];
+var resolvers = {};
+
+for (schema of schemaFiles) {
+    var schemaItem = require(`./schema/${schema}`);
+    typeDefs.push(schemaItem.typeDefs);
+    resolvers = merge(resolvers, schemaItem.resolvers)
+}
 
 const server = new GraphQLServer({
-    typeDefs: [
-        base.typeDefs,
-        user.typeDefs,
-        users.typeDefs,
-    ],
-    resolvers: merge(
-        base.resolvers,
-        user.resolvers,
-        users.resolvers,
-    )
-})
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+});
 
 server.start(() => console.log('Server is running on localhost:4000'))
