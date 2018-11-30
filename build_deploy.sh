eb66b50986c5a264dcf9ad5b804de8662e6a7dba
Jaime Melis <jmelis@redhat.com>
2018-11-30 09:37:27 +0100

    Missing datafile packing

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+set -e
+
 # Required secrets:
 #
-# - AWS_ACCESS_KEY_ID: key to upload file to s3 bucket
-# - AWS_SECRET_ACCESS_KEY: secret key to upload file to s3 bucket
+# - AWS_ACCESS_KEY_ID: aws credentials key id
+# - AWS_SECRET_ACCESS_KEY: aws credentials secret access key
+# - AWS_S3_BUCKET: bucket to access
+# - AWS_S3_KEY: file to upload
 # - USERNAME: username for app-interface.devshift.net
 # - PASSWORD: password for app-interface.devshift.net
 
 # https://vault.devshift.net/ui/vault/secrets/app-sre/show/creds/app-interface-s3-production
 # https://vault.devshift.net/ui/vault/secrets/app-sre/show/creds/app-interface-basic-auth-prod
 
-BUCKET=app-interface-production
-FILE=data.json
+# Install required pip modules
+rm -rf venv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
-aws s3 cp ${FILE} s3://${BUCKET}/${FILE}
+./pack-datafiles.py data > ${AWS_S3_KEY}
+
+aws s3 cp ${AWS_S3_KEY} s3://${AWS_S3_BUCKET}/${AWS_S3_KEY}
 curl -u "${USERNAME}:${PASSWORD}" https://app-interface.devshift.net/reload
 
 exit 0
