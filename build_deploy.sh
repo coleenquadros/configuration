2f1543193451dd98c210fe01af95ddd6991a0fcb
Jaime Melis <jmelis@redhat.com>
2018-12-20 16:18:22 +0100

    update build_deploy.sh logic to push datafiles to stg and prod envs

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,29 +2,73 @@
 
 set -e
 
-# Required secrets:
-#
-# - AWS_ACCESS_KEY_ID: aws credentials key id
-# - AWS_SECRET_ACCESS_KEY: aws credentials secret access key
-# - AWS_S3_BUCKET: bucket to access
-# - AWS_S3_KEY: file to upload
-# - USERNAME: username for app-interface.devshift.net
-# - PASSWORD: password for app-interface.devshift.net
-
-# https://vault.devshift.net/ui/vault/secrets/app-sre/show/creds/app-interface-s3-production
-# https://vault.devshift.net/ui/vault/secrets/app-sre/show/creds/app-interface-basic-auth-prod
-
 source ./.env
 
-# Install required pip modules
-rm -rf venv
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
+wait_response() {
+    local count=0
+    local max=10
 
-./pack-datafiles.py data > ${AWS_S3_KEY}
+    URL=$1
+    EXPECTED_RESPONSE=$2
 
-aws s3 cp ${AWS_S3_KEY} s3://${AWS_S3_BUCKET}/${AWS_S3_KEY}
-curl -u "${USERNAME}:${PASSWORD}" https://app-interface.devshift.net/reload
+    while [[ ${count} -lt ${max} ]]; do
+        let count++
+        RESPONSE=$(curl -sf $URL)
+        [[ "$EXPECTED_RESPONSE" == "$RESPONSE" ]] && break || sleep 10
+    done
+
+    if [[ "$EXPECTED_RESPONSE" != "$RESPONSE" ]]; then
+      echo "Invalid response." >&2
+      echo "Expecting:\n$EXPECTED_RESPONSE" >&2
+      echo "Got:\n$RESPONSE" >&2
+      exit 1
+    fi
+}
+
+# Create data bundle
+
+docker run --rm -v `pwd`/data:/data:z \
+  ${VALIDATOR_IMAGE}:${VALIDATOR_IMAGE_TAG} \
+  qontract-bundler /data > data.json
+
+SHA256=$(sha256sum data.json | awk '{print $1}')
+
+# Upload to staging and reload
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_STAGING
+export AWS_REGION=$AWS_REGION_STAGING
+export AWS_S3_BUCKET=$AWS_S3_BUCKET_STAGING
+export AWS_S3_KEY=$AWS_S3_KEY_STAGING
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_STAGING
+
+export USERNAME=$USERNAME_PRODUCTION
+export PASSWORD=$PASSWORD_PRODUCTION
+
+aws s3 cp data.json s3://${AWS_S3_BUCKET}/${AWS_S3_KEY}
+
+curl "https://${USERNAME}:${PASSWORD}@app-interface.staging.devshift.net/reload"
+
+wait_response \
+    "https://${USERNAME}:${PASSWORD}@app-interface.staging.devshift.net/sha256" \
+    "$SHA256"
+
+# Upload to prodution and reload
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PRODUCTION
+export AWS_REGION=$AWS_REGION_PRODUCTION
+export AWS_S3_BUCKET=$AWS_S3_BUCKET_PRODUCTION
+export AWS_S3_KEY=$AWS_S3_KEY_PRODUCTION
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PRODUCTION
+
+export USERNAME=$USERNAME_PRODUCTION
+export PASSWORD=$PASSWORD_PRODUCTION
+
+aws s3 cp data.json s3://${AWS_S3_BUCKET}/${AWS_S3_KEY}
+
+curl "https://${USERNAME}:${PASSWORD}@app-interface.devshift.net/reload"
+
+wait_response \
+    "https://${USERNAME}:${PASSWORD}@app-interface.devshift.net/sha256" \
+    "$SHA256"
 
 exit 0
