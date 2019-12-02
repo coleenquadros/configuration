5a1d0e6efea089b974e78437970b19fba3294ba7
Maor Friedman <mafriedm@redhat.com>
2019-12-02 07:57:14 +0000

    terraform-resources write output to vault

--- hack/periodic_job.sh
+++ hack/periodic_job.sh
@@ -39,7 +39,7 @@ run_int gitlab-permissions &
 run_int ldap-users $APP_INTERFACE_PROJECT_ID &
 run_int slack-usergroups &
 run_int openshift-resources --internal &
-run_int terraform-resources --internal --light &
+run_int terraform-resources --internal --light --vault-output-path app-sre/integrations-output &
 
 SQS_GATEWAY=true run_int gitlab-pr-submitter $APP_INTERFACE_PROJECT_ID &
 
