361ed3e263c08cc2b2398d12cb689f4a850a838b
Jaime Melis <jmelis@redhat.com>
2019-12-20 12:56:42 +0100

    fix reports

--- hack/app-interface-reporter.sh
+++ hack/app-interface-reporter.sh
@@ -4,9 +4,12 @@ source ./.env
 
 echo "$CONFIG_TOML" | base64 -d > config.toml
 
-docker run --rm -v $PWD/config.toml:/config.toml \
+mkdir -p reports
+docker run --rm \
+    -v $PWD/config.toml:/config.toml \
+    -v $PWD/reports:/reports \
     $RECONCILE_IMAGE:$RECONCILE_IMAGE_TAG \
     app-interface-reporter \
     --config /config.toml \
     --gitlab-project-id 13582 \
-    --reports-path reports
+    --reports-path /reports
