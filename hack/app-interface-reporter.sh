d6be5063f593cdda8019c103df7231295153dc89
Jaime Melis <jmelis@redhat.com>
2019-12-20 12:40:40 +0100

    adds reports path to the reporter

--- hack/app-interface-reporter.sh
+++ hack/app-interface-reporter.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+source ./.env
+
 echo "$CONFIG_TOML" | base64 -d > config.toml
 
 docker run --rm -v $PWD/config.toml:/config.toml \
-    quay.io/app-sre/qontract-reconcile:latest \
+    $RECONCILE_IMAGE:$RECONCILE_IMAGE_TAG \
     app-interface-reporter \
     --config /config.toml \
     --gitlab-project-id 13582 \
