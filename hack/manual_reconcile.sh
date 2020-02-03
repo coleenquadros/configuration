3a591a87013a3c4b9c57f65a357b182d9d365bed
Maor Friedman <mafriedm@redhat.com>
2020-02-03 14:24:45 +0000

    [openshift-serviceaccount-tokens] introduce new integration

--- hack/manual_reconcile.sh
+++ hack/manual_reconcile.sh
@@ -136,6 +136,7 @@ run_int openshift-resources &
 run_int openshift-network-policies &
 run_int openshift-acme &
 run_int openshift-limitranges &
+run_int openshift-serviceaccount-tokens &
 run_int terraform-resources &
 run_int terraform-users &
 run_int ldap-users $APP_INTERFACE_PROJECT_ID &
