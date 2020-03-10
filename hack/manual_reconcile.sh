7651632ad0141e59e0becf5d5dcb1edf2c41166c
Maor Friedman <mafriedm@redhat.com>
2020-03-10 13:52:55 +0000

    hive stage v1 migration

--- hack/manual_reconcile.sh
+++ hack/manual_reconcile.sh
@@ -136,7 +136,8 @@ run_int openshift-resources &
 run_int openshift-network-policies &
 run_int openshift-acme &
 run_int openshift-limitranges &
-run_int openshift-serviceaccount-tokens &
+# uncomment the following line after hive v1 migration is complete (stage)
+# run_int openshift-serviceaccount-tokens &
 run_int openshift-saas-deploy &
 run_int terraform-resources &
 run_int terraform-users &
