9c1903c6b6d5e906c4edc075321bf4ea0e2512b9
Maor Friedman <maor.friedman@redhat.com>
2020-07-01 15:25:38 +0300

    run gitlab-housekeeping once a minute

---
+++ hack/periodic_gitlab_housekeeping.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -exvo pipefail
+
+source ./.env
+
+# Run integrations
+
+# Write config.toml for reconcile tools
+mkdir -p config
+echo "$CONFIG_TOML" | base64 -d > config/config.toml
+
+SUCCESS_DIR=reports/reconcile_reports_success
+FAIL_DIR=reports/reconcile_reports_fail
+LOG_DIR=logs
+rm -rf ${SUCCESS_DIR} ${FAIL_DIR} ${LOG_DIR}; mkdir -p ${SUCCESS_DIR} ${FAIL_DIR} ${LOG_DIR}
+
+set +e
+
+WORK_DIR=`pwd`
+CURRENT_DIR=$(dirname "$0")
+source $CURRENT_DIR/runners.sh
+
+GRAPHQL_SERVER=https://${GRAPHQL_SERVER_BASE_URL}/graphql
+
+run_int gitlab-housekeeping 
+
+print_execution_times
+update_pushgateway
+check_results
