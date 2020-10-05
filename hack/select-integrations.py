d4abf74b474a9d5d027fa3fe51d3b25e0999d96e
Maor Friedman <maor.friedman@redhat.com>
2020-10-05 14:00:43 +0300

    select jenkins-job-cleaner when resources/jenkins is changed

--- hack/select-integrations.py
+++ hack/select-integrations.py
@@ -128,6 +128,7 @@ def main():
         selected.add('terraform-resources')
     if any_modified(lambda p: re.match(r'^resources/jenkins/', p)):
         selected.add('jenkins-job-builder')
+        selected.add('jenkins-job-cleaner')
     if any_modified(lambda p: re.match(r'^resources/', p) \
             and not re.match(r'resources/(terraform|jenkins)/', p)):
         selected.add('openshift-routes')
