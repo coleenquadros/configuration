c5423e7a684ffa7e511d5885f53ff92ecea2db6e
Maor Friedman <maor.friedman@redhat.com>
2021-05-18 11:06:39 +0300

    disable jobs in ci-int

--- hack/periodic_job.sh
+++ hack/periodic_job.sh
@@ -25,25 +25,25 @@ APP_INTERFACE_PROJECT_ID=13582
 
 GRAPHQL_SERVER=https://${GRAPHQL_SERVER_BASE_URL}/graphql
 
-run_int gitlab-projects
+# run_int gitlab-projects
 
 # TODO: move vault integration to run in a pod
 run_vault_reconcile_integration &
 
 # run_int jenkins-plugins &
-run_int jenkins-roles &
-run_int jenkins-webhooks &
-run_int jenkins-webhooks-cleaner &
-run_int gitlab-members &
-run_int gitlab-permissions &
-run_int gitlab-integrations &
-run_int ldap-users $APP_INTERFACE_PROJECT_ID &
+# run_int jenkins-roles &
+# run_int jenkins-webhooks &
+# run_int jenkins-webhooks-cleaner &
+# run_int gitlab-members &
+# run_int gitlab-permissions &
+# run_int gitlab-integrations &
+# run_int ldap-users $APP_INTERFACE_PROJECT_ID &
 # run_int slack-usergroups &
-run_int openshift-resources --internal &
-run_int openshift-vault-secrets --internal &
+# run_int openshift-resources --internal &
+# run_int openshift-vault-secrets --internal &
 # run_int terraform-resources --internal --light --vault-output-path app-sre/integrations-output &
 
-SQS_GATEWAY=true run_int gitlab-mr-sqs-consumer $APP_INTERFACE_PROJECT_ID &
+# SQS_GATEWAY=true run_int gitlab-mr-sqs-consumer $APP_INTERFACE_PROJECT_ID &
 
 wait
 
