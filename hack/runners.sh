4cb2896bab5098ed9791f97aa894091cf982cb1b
Gerd Oberlechner <goberlec@redhat.com>
2022-06-29 09:28:41 +0200

    typo
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- hack/runners.sh
+++ hack/runners.sh
@@ -226,11 +226,11 @@ wait_response() {
 
 upload_s3() {
     INPUT_FILE=$1
-    GIT_COMIIT=$2
+    GIT_COMMIT=$2
 
     SHA256=$(sha256sum $INPUT_FILE | awk '{print $1}')
     aws s3 cp $INPUT_FILE s3://${AWS_S3_BUCKET}/${AWS_S3_KEY}
-    aws s3 cp $INPUT_FILE s3://${AWS_S3_BUCKET}/bundle-archive/${GIT_COMIIT}.json
+    aws s3 cp $INPUT_FILE s3://${AWS_S3_BUCKET}/bundle-archive/${GIT_COMMIT}.json
     wait_response \
         "https://${GRAPHQL_USERNAME}:${GRAPHQL_PASSWORD}@${GRAPHQL_SERVER_BASE_URL}/sha256" \
         "$SHA256"
